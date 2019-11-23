798e11e550aa7c62287544a38710894e9436dfe1
remiodufuye <remi.odufuye@gmail.com>
2019-11-23 15:53:07 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,48 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    params[:name].reverse
+  end
 
+  get '/square/:number' do
+    (params[:number].to_i ** 2).to_s
+  end
+
+  get '/say/:number/:phrase' do
+    solution = ''
+    params[:number].to_i.times do
+      solution += params[:phrase]
+    end
+    solution
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    word1 = params[:word1]
+    word2 = params[:word2]
+    word3 = params[:word3]
+    word4 = params[:word4]
+    word5 = params[:word5]
+    "#{word1} #{word2} #{word3} #{word4} #{word5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    number1 = params[:number1].to_i
+    number2 = params[:number2].to_i
+
+    # answer = 'Unable to perform this operation'
+    solution = 'Cannot Perform this operation'
+
+    case params[:operation]
+    when 'add'
+      solution = (number1 + number2).to_s
+    when 'subtract'
+      solution = (number1 - number2).to_s
+    when 'multiply'
+      solution = (number1 * number2).to_s
+    when 'divide'
+      solution = (number1 / number2).to_s
+    end
+  end
 end
